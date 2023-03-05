ded58b7e9db3e95e8066b930775178501783b2b8
mesor21 <menzdd21@gmail.com>
2023-03-06 01:53:37 +0300

    All tables are created. TODO list for test.

--- src/main/java/com/example/ogrebeauty/Main.java
+++ src/main/java/com/example/ogrebeauty/Main.java
@@ -28,9 +28,32 @@ public class Main extends Application {
     public static void main(String[] args) {
         PostgreSQLJDBC connectionDatabase = new PostgreSQLJDBC();
         connectionDatabase.connectTest();
-        connectionDatabase.autocreateTables();
 
-        EmployeesRepo employeesRepo=new EmployeesRepo();
+        /*TODO
+        test list
+
+        Save data client without mark
+        Save data client with mark
+        Save data employees
+        Save data service with client
+        Save data service with employees
+        Save data service without client and employees
+        Save data service with client and employees
+        Get data service with client
+        Get data service with employees
+        Get data service without client and employees
+        Get data service with client and employees
+        Get data client without mark
+        Get data client with mark
+        Get data employee
+        Delete client with mark
+        Delete service with client
+        Delete service with employees
+        Delete service without client and employees
+
+        */
+
+        /*EmployeesRepo employeesRepo=new EmployeesRepo();
         employeesRepo.saveEmployees(new Employees(Long.parseLong("1"), "Dima M","admin", null));
         employeesRepo.findEmployeesById(Long.parseLong("1"));
         employeesRepo.deleteEmployeesById(Long.parseLong("1"), true);
@@ -43,8 +66,7 @@ public class Main extends Application {
         ServiceRepo serviceRepo = new ServiceRepo();
         serviceRepo.saveService(new Service(Long.parseLong("1"), "12/12/2023 12:12", "test System", null, null));
         serviceRepo.findServiceById(Long.parseLong("1"));
-        serviceRepo.deleteServiceById(Long.parseLong("1"),true);
-
+        serviceRepo.deleteServiceById(Long.parseLong("1"),true);*/
 
         launch();
     }
