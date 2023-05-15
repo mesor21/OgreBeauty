a2c277c1c5caa574c5185edb3038dd2b84879ffe
NotFoundNone <104909568+NotFoundNone@users.noreply.github.com>
2023-05-15 22:34:42 +0300

    fix design

---
+++ src/main/java/com/example/ogrebeauty/controller/DTO/EmployeesDTO.java
@@ -0,0 +1,49 @@
+//package com.example.ogrebeauty.controller.DTO;
+//
+//import com.example.ogrebeauty.entity.Services;
+//
+//public class EmployeesDTO {
+//    private Long id;
+//    private String serviceType;
+//    private String price;
+//    private Services services;
+//
+//    public ServicesDTO(Services services) {
+//        this.services = services;
+//        this.id = services.getId();
+//        this.serviceType = services.getServiceType();
+//        this.price = services.getPrice()+"";
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getServiceType() {
+//        return serviceType;
+//    }
+//
+//    public void setServiceType(String serviceType) {
+//        this.serviceType = serviceType;
+//    }
+//
+//    public String getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(String price) {
+//        this.price = price;
+//    }
+//
+//    public Services getServices() {
+//        return services;
+//    }
+//
+//    public void setServices(Services services) {
+//        this.services = services;
+//    }
+//}
