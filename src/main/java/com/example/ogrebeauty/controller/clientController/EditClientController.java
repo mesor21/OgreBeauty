823a65cff1d85ce2418c596fda4f31042def7caa
mesor21 <menzdd21@gmail.com>
2023-05-16 00:02:16 +0300

    Client edit

--- src/main/java/com/example/ogrebeauty/controller/clientController/EditClientController.java
+++ src/main/java/com/example/ogrebeauty/controller/clientController/EditClientController.java
@@ -1,5 +1,20 @@
 package com.example.ogrebeauty.controller.clientController;
 
-public class EditClientController {
+import com.example.ogrebeauty.controller.helpClass.WindowManager;
+import com.example.ogrebeauty.entity.Client;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
 
+public class EditClientController {
+    private WindowManager windowManager;
+    private ClientController clientController;
+    private Client client;
+    @FXML private TextField fullName;
+    @FXML private TextField email;
+    @FXML private TextField phoneNumber;
+    @FXML private TextField mark;
+    @FXML private Button confirmData;
+    @FXML private Button rejectData;
 }
