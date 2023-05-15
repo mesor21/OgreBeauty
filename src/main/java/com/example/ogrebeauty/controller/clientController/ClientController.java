5598f5e97d09d8eb361006920a88d94b7c22f537
mesor21 <menzdd21@gmail.com>
2023-05-16 00:24:18 +0300

    bugfix

--- src/main/java/com/example/ogrebeauty/controller/clientController/ClientController.java
+++ src/main/java/com/example/ogrebeauty/controller/clientController/ClientController.java
@@ -1,9 +1,34 @@
 package com.example.ogrebeauty.controller.clientController;
 
+import com.example.ogrebeauty.controller.DTO.ClientDTO;
 import com.example.ogrebeauty.controller.helpClass.Controller;
 import com.example.ogrebeauty.controller.helpClass.RedirectController;
+import com.example.ogrebeauty.entity.Client;
+import com.example.ogrebeauty.service.ClientService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.List;
 
 public class ClientController extends RedirectController implements Controller {
+    private ClientService clientService;
+    private ObservableList<ClientDTO> clientDTOObservableList;
+    @FXML public TableView<ClientDTO>  clientTable;
+    @FXML private TableColumn<ClientDTO, String> fullName;
+    @FXML private TableColumn<ClientDTO, String> email;
+    @FXML private TableColumn<ClientDTO, String> phoneNumber;
+    @FXML private TableColumn<ClientDTO, String> editButton;
+    @FXML private TableColumn<ClientDTO, String> deleteButton;
+    private ObservableList<ClientDTO> setObservableList(List<Client> services){
+        ObservableList<ClientDTO> servicesDTOS = FXCollections.observableArrayList();
+        for(int i=0; i<services.size(); i++){
+            servicesDTOS.add(new ClientDTO(services.get(i)));
+        }
+        return servicesDTOS;
+    }
     public void initialize(){
 
     };
--- src/main/java/com/example/ogrebeauty/controller/employeesController/EmployeesController.java
+++ src/main/java/com/example/ogrebeauty/controller/employeesController/EmployeesController.java
@@ -1,27 +1,43 @@
 package com.example.ogrebeauty.controller.employeesController;
 
+import com.example.ogrebeauty.controller.DTO.EmployeesDTO;
 import com.example.ogrebeauty.controller.helpClass.Controller;
 import com.example.ogrebeauty.controller.helpClass.RedirectController;
 import com.example.ogrebeauty.entity.Employees;
 import com.example.ogrebeauty.service.EmployeesService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
+import java.util.List;
+
 public class EmployeesController extends RedirectController implements Controller {
     private EmployeesService employeesService;
+    private ObservableList<EmployeesDTO> employeesDTOObservableList;
+    @FXML
+    public TableView<EmployeesDTO> employeesTable;
+    @FXML
+    private TableColumn<EmployeesDTO, String> fullName;
+    @FXML
+    private TableColumn<EmployeesDTO, String> jobTitle;
+    @FXML
+    private TableColumn<EmployeesDTO, String> editButton;
+    @FXML
+    private TableColumn<EmployeesDTO, String> deleteButton;
+
     public  EmployeesController(){
         employeesService = new EmployeesService();
     }
-    @FXML
-    private TableView<Employees> employeesTable;
-    @FXML
-    private TableColumn<Employees, String> fullName;
-    @FXML
-    private TableColumn<Employees, String> jobTitle;
-    @FXML
-    private TableColumn<Employees, Long> buttons;
 
+    private ObservableList<EmployeesDTO> setObservableList(List<Employees> services){
+        ObservableList<EmployeesDTO> servicesDTOS = FXCollections.observableArrayList();
+        for(int i=0; i<services.size(); i++){
+            servicesDTOS.add(new EmployeesDTO(services.get(i)));
+        }
+        return servicesDTOS;
+    }
     public void initialize(){
 
     };
